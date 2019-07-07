3fce14867b2fb02a7161884f6ca7419b0a8982b8
E.B <edenbenbow@yahoo.com>
2019-07-07 17:31:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function happyHolidays {
   return ("Happy holidays!")
 }
 
-function happyHolidaysTo (name) {
-return (`Happy holidays, ${name}!`)
+function happyHolidaysTo(name) {
+return `Happy holidays, ${name}!`
 }
 
 function happyHolidayTo (holiday) (name) {
